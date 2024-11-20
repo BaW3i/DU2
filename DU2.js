931cec8ecbd393e440aeb478e5cb470b7c773653
Barbora Weigertova <barbora.weigertova@previo.cz>
2024-11-20 22:30:09 +0100

    heh

--- DU2.js
+++ DU2.js
@@ -1,17 +1,38 @@
-/*
-Definuju základní cenu HOTOVO
-Definuju %slevy či příplatku pro dané firmy HOTOVO
 
-1. člověk na webu vybírá
-  1.a) člověk na webu si vybere kateringovou službu
-  1.b) člověk na webu si vybere počet lidí
-  1.c) člověk napíše, co za event že to vlastně má
+/* podle zadání */
 
-2. napíšu fci, která pro každou firmu vrátí na stránku výpočet ceny a oznámení eventu
-3. udělá se event
+const FlavourHavenPrice = 750  ; //cena člověk
+const YourMamaPrice = 500 ;
+const JustFoodPrice = 350 ;
 
-mohli bychom napsat speciální event, který kromě normálních cen třeba bude mít přirážku či slevu? jako předvolba catering na silvestr or what?
- */
+const HavenPicked = (numberOfPeople) => {
+    const havenOrder = numberOfPeople*FlavourHavenPrice;
+    return `you picked Flavour Haven for ${havenOrder} for ${numberOfPeople}`
+}
+
+const YourPicked = (numberOfPeople) => {
+    const yourOrder = numberOfPeople* YourMamaPrice;
+    return `you picked Your Mama for ${yourOrder} for ${numberOfPeople}`
+}
+
+const JustPicked = (numberOfPeople) => {
+    const justOrder = numberOfPeople*JustFoodPrice;
+    return `you picked Just Food for ${justOrder} for ${numberOfPeople}`
+}
+
+const createEvent = (specialDay, numberOfPeople, JustPicked) => {
+    const cateringMessage = JustPicked(numberOfPeople);
+    return `For ${specialDay} ${cateringMessage} people`
+}
+
+console.log(createEvent("Inaugurace prezidenta", 100, HavenPicked));
+console.log(createEvent("Firemní večírek", 50, YourPicked));
+console.log(createEvent("Dětská oslava", 30, JustPicked));
+
+
+
+/* tohle je to samé zadání ale s formulářem pro příchozího člověka do mé smyšlené internetové platformy "CatBooking" (catering booking) poskytující 
+výběr z několika cateringových firem najednou
 
 const setPrice = 1000;
 const FlavourHavenPrice = setPrice;
@@ -53,9 +74,22 @@ numberOfPeopleElement.addEventListener('input', () => {
     }
  }
  
- 
+ /*
+ co by se tu mohlo změnit: 
+ -omezení na počet osob, kterým se dělá catering
+     - buď plošně dané limity platformy nebo každá příchozí cateringová firma mé platformě řekne, kolik max zvládne lidí
+- nebude možné zadat 0 osob
+- co když nějaký fiškus zadá počet osob jako 09 a ne 9?
+- když mám závislý addEventListener na 'change' a jednu z možností v meníčku "vyberte firmu", nebude se mi to na tom kousat? - zabezpečit
+- vytvořit meníčko s významnými dny, které dají slevu nebo příplatek plošně k normální ceně
+- od koho probůh beru objednávky? - kontaktní údaje by mohly být posbírány
+- možnost "přidejte k objednávce poznámku"
+- po dokončení celé registrace objednávky už nemůžu klikat na odeslání znovu a hromadit tak kopie objednávek - přesměrování na jinou stránku?
+- chápu jen mlhavě, donajít: nemají náhodou eventListenery počkat na naběhnutí celé stránky a až pak začít dealovat? - pokud jo, zabezpečit
 
 
+     */
+*/
 
 
 
